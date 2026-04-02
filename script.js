cf2d2150214e837a99c53fcedc71a46b8883e5fd
Saki <sakisakiix@gmail.com>
2026-04-02 22:56:25 +0200

    refresh

--- script.js
+++
@@ -1,8 +0,0 @@
-const url = "https://movie.pequla.com/api/movie"
-
-fetch(url)
-    .then(rsp => rsp.json()
-    .then(data => {
-        const movies = data.movies
-        const movieList = document.getElementById('movie-title')
-    }))

3f797d15fae06871d64c49558664bef237ed7fcb
Saki <sakisakiix@gmail.com>
2026-04-02 23:35:05 +0200

    startingPoint

---
+++ script.js
@@ -0,0 +1,9 @@
+const url = "https://movie.pequla.com/api/movie"
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const movies = data.movies;
+        const moviesContainer = document.getElementById("container")
+    })
+    moviesContainer.innerHTML = movies.data

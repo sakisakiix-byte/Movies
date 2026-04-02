4cb10722195859fe45874f7d6e05a9cdc7ecd84e
Saki <sakisakiix@gmail.com>
2026-04-02 23:43:23 +0200

    test

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const url = "https://movie.pequla.com/api/movie"
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        const movies = data.movies;
-        const moviesContainer = document.getElementById("container")
-        console.log(movies)
+        const movies = document.getElementById('movies')
+        for(let movie of data){
+            const list = document.createElement('li')
+            list.innerText = movie.title 
+            movies.appendChild(list)       
+        }
+        
+        
     })
